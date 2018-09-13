190778911bbad88d3867ebedb735aa212e62e500
SandraMT <43126177+SandraMT@users.noreply.github.com>
2018-09-13 09:09:14 -0500

    SemaforoVP_boton_final

--- Practica1_SemaforoVP.ino
+++ Practica1_SemaforoVP.ino
@@ -1,4 +1,3 @@
-
 #define led_verde 13
 #define led_amarillo 12
 #define led_rojo 11
@@ -12,29 +11,60 @@
 
 int valor_pote = 0;
 
+int boton1 = 6;
+int push1 = 0;
+int boton2 = 5;
+int push2 = 0 ;
+int control = 0;
+
+
 void setup() {
   // put your setup code here, to run once:
   pinMode(led_verde,OUTPUT);
   pinMode(led_amarillo, OUTPUT);
   pinMode(led_rojo,OUTPUT);
-
   pinMode(verde1, OUTPUT);
   pinMode(rojo1, OUTPUT);
   pinMode(verde2, OUTPUT);
   pinMode(rojo2, OUTPUT);
- 
+  
+  pinMode(boton1, INPUT);
+  pinMode(boton2, INPUT);
+
+  Serial.begin(9600); //--------------- efectos de comprobacion funcion del potenciometro
+
 }
 
 void loop() { 
+
+ push1 = LOW;//boton1
+ push2 = LOW; // boton2
+
 valor_pote = analogRead(pote);
+//cambio = map(valor_pote,0,1023,10,60)*1000; //-------
 
 Serial.println(valor_pote);
    
   verde();
-  preventivaVerde();
-  amarillo();   // al activarse se ve la funcionalidad normal de semaforos vehicular y peatonal
-  rojo(); 
+  //preventivaVerde();
+ // amarillo();   // al activarse se ve la funcionalidad normal de semaforos vehicular y peatonal
+//  rojo(); 
   
+  push1 = digitalRead(boton1);
+  push2 = digitalRead(boton2);
+  
+  if(push1 == LOW || push2 == LOW) {
+
+    control = 1; 
+    }else {control = 0;} 
+    
+    if (control == 1) {
+      preventivaVerde();
+      amarillo();
+      rojo();    
+    } else { if (control = 0) { verde();}}
+    
+} 
  
 /* S E M A F O R O */
 void verde (){
@@ -45,15 +75,17 @@ void verde (){
   digitalWrite(led_verde,HIGH);   //alto
   delay(valor_pote*17.05); //31500 original valor_pote
 
+  digitalWrite(led_verde, LOW);
 }
 
 void preventivaVerde (){
+  
   digitalWrite(rojo1,HIGH); //alto peaton
   digitalWrite(rojo2,HIGH);//-------
   
-    //verde
+  //verde
   digitalWrite(led_verde,HIGH);   //alto
-  delay(valor_pote*17.05); //31500 original valor_pote
+  delay(valor_pote*17.05); //31500 original
 
   digitalWrite(led_verde, LOW);
     //parpadeo
@@ -71,20 +103,19 @@ void preventivaVerde (){
 void amarillo (){
    //amarillo
   digitalWrite(led_amarillo, HIGH);  //alto
-  delay(3000); //3000 
+  delay(3000); //3000 ----------------
 
   digitalWrite(led_amarillo,LOW); 
 }
 
 void rojo (){
-  
   //rojo sem
   digitalWrite(led_rojo, HIGH); //alto vehiculo
+  
   delay(3000);
-
   digitalWrite(rojo1,LOW);  //peaton
   digitalWrite(rojo2,LOW); //peaton
-
+  
     
 //paso peaton
   digitalWrite(verde1,HIGH);
@@ -104,13 +135,15 @@ for(int i = 0; i<3; i++){
   delay(500);
 }
 
-  digitalWrite(rojo1,HIGH); //peaton tiempo espera -> vehiculo y peaton en rojo
+  
+  digitalWrite(rojo1,HIGH); //peaton tiempo espera vehiculo y peaton en rojo
   digitalWrite(rojo2,HIGH);// peaton
-
-  delay(3000);  // ambos en rojo po seguridad  
+  
+  delay(3000);
   digitalWrite(led_rojo,LOW); //bajo
 
-    
+  
   digitalWrite(verde1,LOW); //peaton---------
   digitalWrite(verde2,LOW);//-------
+
 }
